f18d081a8063db8be9fb5805913a40a3c24387c0
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2021-05-20 16:34:42 +0100

    Update BottomNavigator.js

--- components/BottomNavigator.js
+++ components/BottomNavigator.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View,Image } from 'react-native';
 import { createAppContainer,createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import HomeScreen from '../screens/HomeScreen'
-import ExchangeScreen from '../screens/ExchangeScreen'
-import LoginScreen from '../screens/LoginScreen'
+import HomeScreen from '../screens/HomeScreen';
+import ExchangeScreen from '../screens/ExchangeScreen';
+import LoginScreen from '../screens/WelcomeScreen';
 
 export const BottomNavigator = createBottomTabNavigator({
    List:{
